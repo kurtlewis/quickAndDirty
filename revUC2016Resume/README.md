740f0e305654f921e6dfdae290787b2c2e42c5b6
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-10-17 16:28:06 -0700

    Create script to organize resume links

---
+++ revUC2016Resume/README.md
@@ -0,0 +1,3 @@
+#Resume Script
+
+This script was created to generate two lists of links, one for those who did attend the hackathon, and one for those who didn't.
---
+++ revUC2016Resume/resumes.py
@@ -0,0 +1,39 @@
+import openpyxl;
+#load two workbooks
+checkin = openpyxl.load_workbook('sheet1.xlsx')
+resume = openpyxl.load_workbook('sheet2.xlsx')
+wb = openpyxl.Workbook()
+ws = wb.active
+# get the first sheet of each workbook
+checkin = checkin[checkin.get_sheet_names()[0]]
+resume = resume[resume.get_sheet_names()[0]]
+
+checkinNames = list()
+resumeNames = list()
+#Column 4 of checkin is first names, column 5 is last names. Join for full names
+for i in range(2, 223):
+    name = checkin.cell(row = i, column = 4).internal_value + " " + checkin.cell(row = i, column = 5).internal_value
+    name = name.lower()
+    checkinNames.append(name)
+
+for i in range(1, 241):
+    name = resume.cell(row=i, column=1).internal_value
+    name = name.lower()
+    resumeNames.append(name)
+
+attendedLinks = list()
+noAttendedLinks = list()
+for i in range(len(resumeNames)):
+    link = resume.cell(row=i + 1, column=2).internal_value
+    if resumeNames[i] in checkinNames:
+        attendedLinks.append(link)
+    else:
+        noAttendedLinks.append(link)
+
+for i in range(len(attendedLinks)):
+    ws['A'+str(i+1)] = attendedLinks[i]
+
+for i in range(len(noAttendedLinks)):
+    ws['I'+str(i+1)] = noAttendedLinks[i]
+
+wb.save('result.xlsx')

e103f84584c6dbbfdd2be25216e670e065daaf75
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-06-20 15:36:23 -0400

    Add output functionality
    Add Readme
    
    Update format of websites.csv

---
+++ AlisMarketingScript/README.txt
@@ -0,0 +1,23 @@
+INSTRUCTIONS
+
+First, you'll need to have downloaded Python3 if you don't have it installed.
+You can do so here: https://www.python.org/downloads/
+Python3 is a version of Python, a popular scripting programming language. It's safe, and you might already have it on your computer
+When installing, the defaults are probably fine. Just make sure to associate '.py' with Python if it asks
+
+You'll need to finish filling in the data for the 'websites.csv' file.
+
+MAKE SURE TO FOLLOW THE HEADER AND PUT DATA IN THE CORRECT COLUMNS.
+
+
+To run the program, just run 'script.py'.
+The script should auto run when double clicked. If not, right click and select run.
+
+An output window will pop up, the script will scrape the websites and output the results to a bunch of '.csv' files.
+
+You can run the script whenever you want, it will append a new row to each '.csv'. 
+
+NOTE
+If you edit and of the output csv's in their original form, the program won't respond. So don't reorder columns or insert charts - copy the file and make changes on the copy.
+
+Excel will probably ask you everytime you save to save the '.csv's' in an excel format. Don't. Changing the extension will break things.
--- AlisMarketingScript/script.py
+++ AlisMarketingScript/script.py
@@ -9,14 +9,21 @@ If it was, it wouldn't be in the https://github.com/kurtlewis/quickanddirty repo
 import urllib.request
 import csv
 import re
-
-
+import time
+import os.path
 """
 Get's HTML of page for parsing
 """
-def getHtml(url):
-    return str(urllib.request.urlopen(url).read())
-
+def getHtml(url, trys=0):
+    try:
+        return str(urllib.request.urlopen(url).read())
+    except:
+        print("Error occured while reading " + url)
+        if trys < 3:
+            print("Trying again")
+            return getHtml(url, trys=trys+1)
+        else:
+            return ""
 """
 Searches string using the given compiled regex and str
 Returns only the first matching group
@@ -26,14 +33,23 @@ def searchStr(regex, string):
         return regex.search(string).group(1)
     except AttributeError:
         print("Could not find Regex!")
-        return None
+        return "ERROR"
 
 
 """
 Appends to CSV of data
 """
-#def appendToCsv(csvFile):
-
+def writeToCsv(company, outputRow):
+    csvFile = company+"Output.csv"
+    header = ['Date', 'Twitter Followers', 'Twitter Tweets', 'Facebook Likes', 'Facebook Followers', 'Facebook Posts', 'Instagram Followers', 'Instagram Posts', 'Youtube Subscribers', 'Youtube Videos', 'Pinterest Followers']
+    if not os.path.exists(csvFile):
+        with open(csvFile, 'w') as output:
+            outputWriter = csv.writer(output, delimiter=',', quotechar='"')
+            outputWriter.writerow(header)
+    
+    with open(csvFile, 'a') as output:
+        outputWriter = csv.writer(output, delimiter=',', quotechar='"')
+        outputWriter.writerow(outputRow)
 
 def main():
     twitterFollowers = re.compile("title=\"([\d,]*) Followers")
@@ -47,30 +63,38 @@ def main():
     #pinterestFollowers = re.compile(">([\d,]*)</span> <span class='label '>Followers") # Doesn't work?
     with open('websites.csv', 'r') as csvfile:
         websiteReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(websiteReader) # skip header row
         for row in websiteReader:
-            for url in row:
+            output = list()
+            company = row[0]
+            print("Now scraping for " + company)
+            output.append(time.strftime('%d %b %Y, %H:%M', time.gmtime()))
+            for url in row[1:]:
                 page = getHtml(url)
                 if 'twitter' in url:
                     followers = searchStr(twitterFollowers, page)
                     tweets = searchStr(twitterTweets, page)
-                    print(followers)
-                    print(tweets)
+                    output.append(followers)
+                    output.append(tweets)
                 if 'facebook' in url:
                     likes = searchStr(facebookLikes, page)
                     followers = searchStr(facebookFollowers, page)
-                    print(likes)
-                    print(followers)
+                    output.append(likes)
+                    output.append(followers)
+                    output.append("NO DATA") # No data for facebook post numbers
                 if 'instagram' in url:
                     followers = searchStr(instagramFollowers, page)
                     posts = searchStr(instagramPosts, page)
-                    print(followers)
-                    print(posts)
+                    output.append(followers)
+                    output.append(posts)
                 if 'youtube' in url:
                     subscribers = searchStr(youtubeSubscribers, page)
-                    print(subscribers)
+                    output.append(subscribers)
+                    output.append("NO DATA") # No data for youtube video numbers
                 if 'pinterest' in url:
                     followers = searchStr(pinterestFollowers, page)
-                    print(followers)
+                    output.append(followers)
+            writeToCsv(company, output)
 
 
 if __name__ == "__main__":

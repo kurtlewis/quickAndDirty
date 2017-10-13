e8fec42990a3e6ff3fd92471cc1fd86d5af7bc0a
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-10-12 23:13:26 -0400

    Add more instructions to ambassadors json reader

--- ambassadorsJsonInterpreter/readJson.py
+++ ambassadorsJsonInterpreter/readJson.py
@@ -1,21 +1,30 @@
 """
 Kurt Lewis
-Reading data from .json file.
+===================================
+EXTRACTING DATA FROM DUMP.TAR
+$tar -xf <name of file>
+$bsondump members.bson > memberInfo.json
+$cp <path>/memberInfo.json <new path>/memberInfo.json
+====================================
+READING DATA FROM JSON FILE
 Remember that the data I get direct from the database is gross. I probably need to put it in an array and add comma delimiters.
 I did that using this direct from the python CLI
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 file = open("memberInfo.json", 'r')
 content = file.read()
 file.close()
-content = content.close("}\n{" "},\n{")
+content = content.close("}\n{", "},\n{")
 file = open("memberInfo2.json", 'w')
 file.write(content)
 file.close()
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Then wrap everything in a [] and delete any partial objects
 """
 import json
 
 print("Name,Email,Minutes")
 
-with open("memberInfo.json", 'r') as data_file:
+with open("memberInfo2.json", 'r') as data_file:
     data = json.loads(data_file.read())
     for idData in data:
         print(str(idData['profile']['firstName'] + " " + str(idData['profile']['lastName'])), end=",")
